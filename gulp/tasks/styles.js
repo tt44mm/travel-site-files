16d172a4b854a5819fc16d7890e680a2e01b46c9
Thomas <tmenath@gmail.com>
2018-12-08 11:01:41 +0100

    header halfway

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,12 +5,13 @@ var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 
 gulp.task('cssstyles', function() {
 
     return gulp.src('./app/assets/css/style.css')
-        .pipe(postcss([cssimport, mixins, nested, cssvars, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, nested, cssvars, hexrgba,autoprefixer]))
         .on('error',
             function(errorInfo) {
                 console.log(errorInfo.toString());
